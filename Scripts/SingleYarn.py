ccd5a3ad9a210d4429a31a6c16e33a2a7b5308f7
Louise Brown <louise.brown@nottingham.ac.uk>
2019-10-23 13:37:59 +0100

    Added yarn sections slide to Python section

---
+++ Scripts/SingleYarn.py
@@ -0,0 +1,32 @@
+# SingleYarn.py
+
+#Create an object of the CTextile class 
+textile = CTextile( )
+
+#Create a single yarn object:
+yarn = CYarn( )
+
+#Add 3 nodes to the yarn, specifying the coordinate positions only:
+yarn.AddNode( CNode( XYZ(0, 0.0, 0)) )
+yarn.AddNode( CNode( XYZ(1, 3.5, 0)) )
+yarn.AddNode( CNode( XYZ(0, 7.0, 0)) )
+
+# Set interpolation, defaults to periodic
+yarn.AssignInterpolation( CInterpolationCubic())
+
+# Add repeat vectors
+yarn.AddRepeat( XYZ(3, 0, 0) )
+yarn.AddRepeat( XYZ(0, 7, 0) )
+yarn.AddRepeat( XYZ(0.5, 0, 1) )
+
+# Add the yarn to the textile 
+textile.AddYarn( yarn )	
+
+Plower = XYZ(-0.5, 0, -0.5)
+Pupper = XYZ(7.5, 35, 2.5)
+textile.AssignDomain( CDomainPlanes(Plower, Pupper) )
+
+
+# Add the textile with the name "single yarn" to the TexGen database
+# If the script is run from within the TexGen GUI the yarn is displayed
+AddTextile("single yarn", textile)
---
+++ Scripts/SingleYarnHybrid.py
@@ -0,0 +1,47 @@
+# SingleYarn.py
+
+#Create an object of the CTextile class 
+textile = CTextile( )
+
+#Create a single yarn object:
+yarn = CYarn( )
+
+#Add 3 nodes to the yarn, specifying the coordinate positions only:
+yarn.AddNode( CNode( XYZ(0, 0.0, 0)) )
+yarn.AddNode( CNode( XYZ(1, 3.5, 0)) )
+yarn.AddNode( CNode( XYZ(0, 7.0, 0)) )
+
+# Set interpolation, defaults to periodic
+yarn.AssignInterpolation( CInterpolationCubic())
+
+#Define three different cross-sectional shapes at the master nodes 
+sections = CYarnSectionInterpNode()
+sections.AddSection(CSectionLenticular(1, 0.5, 0.1))
+sections.AddSection(CSectionPowerEllipse( 1.0, 0.4, 0.4, 0.25) )
+
+Hybrid = CSectionHybrid()
+Hybrid.AddDivision(0)
+Hybrid.AddDivision(0.3)
+Hybrid.AssignSection(0, CSectionLenticular(1.0,0.4))
+Hybrid.AssignSection(1, CSectionEllipse(1.0,0.4))
+sections.AddSection(Hybrid)
+
+# Assign sections to yarn
+yarn.AssignSection( sections )
+
+# Add repeat vectors
+yarn.AddRepeat( XYZ(3, 0, 0) )
+yarn.AddRepeat( XYZ(0, 7, 0) )
+yarn.AddRepeat( XYZ(0.5, 0, 1) )
+
+# Add the yarn to the textile 
+textile.AddYarn( yarn )	
+
+Plower = XYZ(-0.5, 0, -0.5)
+Pupper = XYZ(7.5, 35, 2.5)
+textile.AssignDomain( CDomainPlanes(Plower, Pupper) )
+
+
+# Add the textile with the name "single yarn" to the TexGen database
+# If the script is run from within the TexGen GUI the yarn is displayed
+AddTextile("yarn sections hybrid", textile)
